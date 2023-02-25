cba366dc39ee9a92831f11101b2ba4a34edbdb37
Chris Ohk <utilforever@gmail.com>
2023-02-25 11:16:41 +0900

    feat(window): Add code to create window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use glfw::{OpenGlProfileHint, WindowHint};
+
 fn main() {
-    println!("Hello, world!");
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+    glfw.window_hint(WindowHint::ContextVersionMajor(4));
+    glfw.window_hint(WindowHint::ContextVersionMinor(6));
+    glfw.window_hint(WindowHint::OpenGlProfile(OpenGlProfileHint::Core));
+    glfw.window_hint(WindowHint::OpenGlDebugContext(true));
+
+    let window_size = (500, 500);
+    let window_title = "Minecraft";
+
+    let (window, _) = glfw
+        .create_window(
+            window_size.0,
+            window_size.1,
+            window_title,
+            glfw::WindowMode::Windowed,
+        )
+        .expect("Failed to create GLFW window");
+
+    while !window.should_close() {
+        glfw.poll_events();
+    }
 }
