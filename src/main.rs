e7dd26912b64c9768283d2c627b779524cd5c1b0
Chris Ohk <utilforever@gmail.com>
2023-03-06 23:35:41 +0900

    feat(window): Add code to make the window's context current

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use glfw::{OpenGlProfileHint, WindowHint};
+use glfw::{Context, OpenGlProfileHint, WindowHint};
 
 fn main() {
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
@@ -10,7 +10,8 @@ fn main() {
     let window_size = (500, 500);
     let window_title = "Minecraft";
 
-    let (window, _) = glfw
+    // Create a windowed mode window and its OpenGL context
+    let (mut window, _) = glfw
         .create_window(
             window_size.0,
             window_size.1,
@@ -18,6 +19,12 @@ fn main() {
             glfw::WindowMode::Windowed,
         )
         .expect("Failed to create GLFW window");
+
+    // Make the window's context current
+    window.make_current();
+    window.set_key_polling(true);
+    window.set_cursor_pos_polling(true);
+    window.set_raw_mouse_motion(true);
 
     while !window.should_close() {
         glfw.poll_events();
